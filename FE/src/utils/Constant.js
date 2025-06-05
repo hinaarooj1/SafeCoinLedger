ebf2f56cef42f8e0dc49357fbfd4bb301f84c3e5
hinaarooj1 <hinaarooj978@gmail.com>
2025-06-05 18:21:11 +0500

    sas

--- FE/src/utils/Constant.js
+++ FE/src/utils/Constant.js
@@ -3,6 +3,6 @@ module.exports = {
   // baseUrl: "http://localhost:4000/api/v1",
   // Locala server
   // Official server 
-  // baseUrl: "https://bitblaze.onrender.com/api/v1",
+  baseUrl: "https://safecoinledger.onrender.com/api/v1",
   // Official server
 };
